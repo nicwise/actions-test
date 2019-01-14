2cf454d6bf37b343ce8044688cfcc791a3470a82
Nic Wise <nic.wise@pushpay.com>
2019-01-15 08:26:42 +1300

    with the add and remove

--- tag-on-review/tag-on-review.sh
+++ tag-on-review/tag-on-review.sh
@@ -39,39 +39,22 @@ main(){
     
 
 	if [[ "$action" == "submitted" ]] ; then
-        echo "DEBUG: sbumitted"
-        # ref=$(jq --raw-output .pull_request.head.ref "$GITHUB_EVENT_PATH")
-		# owner=$(jq --raw-output .pull_request.head.repo.owner.login "$GITHUB_EVENT_PATH")
-		# repo=$(jq --raw-output .pull_request.head.repo.name "$GITHUB_EVENT_PATH")
+        ref=$(jq --raw-output .pull_request.head.ref "$GITHUB_EVENT_PATH")
+		owner=$(jq --raw-output .pull_request.head.repo.owner.login "$GITHUB_EVENT_PATH")
+		repo=$(jq --raw-output .pull_request.head.repo.name "$GITHUB_EVENT_PATH")
 
-        # curl -sSL \
-        #     -H "${AUTH_HEADER}" \
-		# 	-H "${API_HEADER}" \
-        #     "${URI}/repos/${owner}/${repo}/issues/"
+        curl -XDELETE -sSL \
+            -H "${AUTH_HEADER}" \
+			-H "${API_HEADER}" \
+            "${pr_url}/labels/ready+for+review"
 
-        # # We only care about the closed event and if it was merged.
-        # # If so, delete the branch.
-	
-		# default_branch=$(
- 		# 	curl -XGET -sSL \
-		# 		-H "${AUTH_HEADER}" \
- 		# 		-H "${API_HEADER}" \
-		# 		"${URI}/repos/${owner}/${repo}" | jq .default_branch
-		# )
+         curl -XPOST -sSL \
+            -H "${AUTH_HEADER}" \
+			-H "${API_HEADER}" \
+            --data '{"labels": "ready to land"}' \
+            "${pr_url}/labels/ready+for+review"
 
-		# if [[ "$ref" == "$default_branch" ]]; then
-		# 	# Never delete the default branch.
-		# 	echo "Will not delete default branch (${default_branch}) for ${owner}/${repo}, exiting."
-		# 	exit 0
-		# fi
-
-		# echo "Deleting branch ref $ref for owner ${owner}/${repo}..."
-		# curl -XDELETE -sSL \
-		# 	-H "${AUTH_HEADER}" \
-		# 	-H "${API_HEADER}" \
-		# 	"${URI}/repos/${owner}/${repo}/git/refs/heads/${ref}"
-
-		# echo "Branch delete success!"
+        echo "all done"
 	fi
 }
 
