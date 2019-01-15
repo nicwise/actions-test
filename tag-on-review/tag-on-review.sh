81b90cf3c24b3b53ab0ff82e0d2e40d2b69e0a76
Nic Wise <nic.wise@pushpay.com>
2019-01-15 15:06:59 +1300

    bit more cleanup

--- tag-on-review/tag-on-review.sh
+++ tag-on-review/tag-on-review.sh
@@ -4,14 +4,14 @@ set -o pipefail
 
 # This is populated by our secret from the Workflow file.
 if [[ -z "$GITHUB_TOKEN" ]]; then
-	echo "Set the GITHUB_TOKEN env variable."
-	exit 1
+    echo "Set the GITHUB_TOKEN env variable."
+    exit 1
 fi
 
 # This one is populated by GitHub for free :)
 if [[ -z "$GITHUB_REPOSITORY" ]]; then
-	echo "Set the GITHUB_REPOSITORY env variable."
-	exit 1
+    echo "Set the GITHUB_REPOSITORY env variable."
+    exit 1
 fi
 
 URI=https://api.github.com
@@ -24,39 +24,41 @@ main(){
     # populated. This file holds the JSON data for the event that was triggered.
     # From that we can get the status of the pull request and if it was merged.
     # In this case we only care if it was closed and it was merged.
+    
+    action=$(jq --raw-output .action "$GITHUB_EVENT_PATH")
+    pr_url=$(jq --raw-output .pull_request.url "$GITHUB_EVENT_PATH")
 
-    cat $GITHUB_EVENT_PATH
-
-	action=$(jq --raw-output .action "$GITHUB_EVENT_PATH")
-	pr_url=$(jq --raw-output .pull_request.url "$GITHUB_EVENT_PATH")
-
-	echo "DEBUG -> action: $action merged: $pr_url"
+    echo "DEBUG -> action: $action merged: $pr_url"
+    ref=$(jq --raw-output .pull_request.head.ref "$GITHUB_EVENT_PATH")
+    owner=$(jq --raw-output .pull_request.head.repo.owner.login "$GITHUB_EVENT_PATH")
+    repo=$(jq --raw-output .pull_request.head.repo.name "$GITHUB_EVENT_PATH")
+    issue_url=$(jq --raw-output .pull_request._links.issue.href "$GITHUB_EVENT_PATH")
 
     if [[ "$action" == "dismissed" ]] ; then
-      echo "DEBUG: do dismissed later"
+        curl -XDELETE -sSL \
+            -H "${AUTH_HEADER}" \
+            -H "${API_HEADER}" \
+            "${issue_url}/labels/ready%20to%20land"
+
+        echo "submit all done"
     fi
 
     
 
-	if [[ "$action" == "submitted" ]] ; then
-        ref=$(jq --raw-output .pull_request.head.ref "$GITHUB_EVENT_PATH")
-		owner=$(jq --raw-output .pull_request.head.repo.owner.login "$GITHUB_EVENT_PATH")
-		repo=$(jq --raw-output .pull_request.head.repo.name "$GITHUB_EVENT_PATH")
-        issue_url=$(jq --raw-output .pull_request._links.issue.href "$GITHUB_EVENT_PATH")
-
+    if [[ "$action" == "submitted" ]] ; then
         curl -XDELETE -sSL \
             -H "${AUTH_HEADER}" \
-			-H "${API_HEADER}" \
+            -H "${API_HEADER}" \
             "${issue_url}/labels/ready%20for%20review"
 
          curl -XPOST -sSL \
             -H "${AUTH_HEADER}" \
-			-H "${API_HEADER}" \
+            -H "${API_HEADER}" \
             --data '{"labels": ["ready to land"]}' \
             "${issue_url}/labels"
 
-        echo "all done"
-	fi
+        echo "submit all done"
+    fi
 }
 
 main "$@"
